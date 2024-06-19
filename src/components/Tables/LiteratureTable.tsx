897897f2dfebe78699d83ea096e0be023abbf580
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-19 16:01:52 +0530

    updated

--- src/components/Tables/LiteratureTable.tsx
+++ src/components/Tables/LiteratureTable.tsx
@@ -2,8 +2,11 @@ import { useState, useEffect } from "react";
 import { apiLink } from "../../api_link";
 import axios from "axios";
 import Swal from 'sweetalert2';
-import DataTable, { createTheme } from 'react-data-table-component';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
+import { InputSwitch } from 'primereact/inputswitch';
 import { Link } from "react-router-dom";
+import Notify from "../toast_notify/Notify";
 
 
 function LiteratureTable() {
@@ -59,103 +62,66 @@ function LiteratureTable() {
         }
     }
 
+    const handleStatus = async(id) => {
+        try{
+            const response = await axios.put(`${apiLink}literature/status`,{
+                data: { id: id }
+            });
+            fetchData()
+            Notify(response.data.message)
+        }
+        catch(err){
+            console.error(err)
+        }
+    }
+
     const handleEdit = async (id: BigInteger) => {
         console.log(id);
     }
 
-    createTheme('solarized', {
-        text: {
-            primary: '#fff',
-            secondary: '#fff',
-        },
-        background: {
-            default: '#24303f',
-        },
-        context: {
-            background: '#cb4b16',
-            text: '#FFFFFF',
-        },
-        divider: {
-            default: '#fff',
-        },
-        action: {
-            button: 'rgba(0,0,0,.54)',
-            // button: "red",
-            hover: 'rgba(0,0,0,.08)',
-            disabled: 'rgba(0,0,0,.12)',
-        },
-    }, 'dark');
-
-    const columns = [
-        {
-            name: 'Category',
-            selector: row => row.category_id,
-        },
-        {
-            name: 'Sub-Category',
-            selector: row => row.sub_category_id,
-        },
-        {
-            name: 'Literature Name (ENG)',
-            selector: row => row.literature_english,
-        },
-        {
-            name: 'Literature Name (MAR)',
-            selector: row => row.literature_marathi,
-        },
-        {
-            name: 'Literature Description (ENG)',
-            selector: row => row.literature_description_english,
-        },
-        {
-            name: 'Author Name (ENG)',
-            selector: row => row.author_name_english,
-        },
-
-        {
-            name: 'Author Name (MAR)',
-            selector: row => row.author_name_marathi,
-        },
-
-        {
-            name: "Saint Name (ENG)",
-            selector: row => row.saint_name_english,
-        },
-        {
-            name: "Saint Name (MAR)",
-            selector: row => row.saint_name_marathi,
-        },
-        {
-            name: "literature content",
-            selector: row => row.literature_content
-        },
-
-
-        {
-            name: "Actions",
-            cell: (row) => (
-                <div className="flex gap-4">
-                    <Link to={`/edit-literature/${row.id}`} className="bg-primary px-2 py-1 rounded-md" >Edit</Link>
-                    <button className="bg-danger px-2 py-1 rounded-md" onClick={() => handleDelete(row.id)}>Delete</button>
-                </div>
-            )
-        }
-    ];
+   
 
     return (
         <>
             <div className="float-right mb-4">
                 <Link to="/add-literature" className="bg-primary text-white px-3 py-2 rounded-md hover:opacity-65">Add</Link>
             </div>
-            <DataTable
-                // title="Books"
-                pagination
-                columns={columns}
-                fixedHeader
-                data={apiData}
-                highlightOnHover
-                theme="solarized"
-            />
+            <br />
+            <br />
+            <DataTable  paginatorClassName={"dark:bg-[#243141] dark:text-[#fff]"} value={apiData} className="shadow-xl" stripedRows paginator rows={10}
+                rowsPerPageOptions={[5, 10, 25, 50]}
+                tableStyle={{ minWidth: '150rem' }}>
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="id" header="Sr.No" body={(item, key) => (
+                    <>
+                        <span>{key.rowIndex + 1}</span>
+                    </>
+                )} ></Column>
+
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="literature_english" header="Literature English" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="literature_marathi" header="Literature Marathi" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="literature_description_english" header="Literature Description English" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="literature_description_marathi" header="Literature Description Marathi" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="author_name_english" header="Author Name English" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="author_name_marathi" header="Author Name Marathi" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="saint_name_english" header="Saint Name English" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="saint_name_marathi" header="Saint Name Marathi" />
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="literature_content" header="Literature Content" />
+                
+
+
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="status" header="Status" body={(rowData) => (
+                    <InputSwitch className="p-invalid" checked={rowData.status} onClick={() => handleStatus(rowData.id)} />
+                )
+                }>
+                </Column>
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="id" header="Action" body={(rowData) => (
+                    <div className="flex gap-2">
+                        <Link to={`/edit-category/${rowData.id}`} className="bg-primary opacity-90  p-2     text-white rounded-full hover:opacity-100 "><i className="fa-solid fa-pen"></i></Link>
+                        <span onClick={() => handleDelete(rowData.id)} className="bg-red-700 opacity-90 hover:opacity-100 p-2  rounded-full text-white" ><i className="fa-solid fa-trash"></i></span>
+                    </div>
+                )} />
+
+            </DataTable>
 
 
         </>
