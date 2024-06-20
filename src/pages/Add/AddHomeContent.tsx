aa97bf1d4e04ad794e7714b06c33da13c6fa9d69
lalitharwateappdid <lalitharwateappdid@gmail.com>
2024-06-20 22:44:54 +0100

    updated

--- src/pages/Add/AddHomeContent.tsx
+++ src/pages/Add/AddHomeContent.tsx
@@ -12,29 +12,29 @@ const AddHomeContent = () => {
 
     const [description, setDescription] = useState("");
     const [image, setImage] = useState("");
-    const [submit,setSubmit] = useState("Submit")
-  
-    function handleImage(e){
+    const [submit, setSubmit] = useState("Submit")
+
+    function handleImage(e) {
         console.log(e.target.files);
         setImage(e.target.files[0])
     }
 
-    function handleDescription(e){
+    function handleDescription(e) {
         console.log(e.target.value);
         setDescription(e.target.value)
     }
     // function to store books
     const createMedia = async () => {
-       
+
         try {
             const formData = new FormData()
-            formData.append("image_path",image);
-            formData.append("description",description);
+            formData.append("image_path", image);
+            formData.append("description", description);
             setSubmit("Submitting...")
             const response = await axios.post(`${apiLink}home-content/create`, formData,
                 {
-                    headers:{
-                        "Content-Type":"multipart/form-data"
+                    headers: {
+                        "Content-Type": "multipart/form-data"
                     }
                 }
             );
@@ -61,34 +61,33 @@ const AddHomeContent = () => {
 
             <DefaultLayout>
                 <Breadcrumb pageName="Add Home Content" />
-                <div className="bg-[#fff] rounded-lg px-5 py-3 shadow-xl dark:bg-transparent">
-                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
-                <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Description
-                        </label>
-                        <textarea rows="1"  onChange={handleDescription}  placeholder="Enter Description" class="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"></textarea>
-                   </div>
-                  
-                    <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Link
-                        </label>
-                        <input
-                            type="file"
-                            placeholder="Upload Image"
-                            onChange={handleImage}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                    </div>
-                   
-                </div>
-                <div className="w-50 mx-auto mt-5">
-                    <button onClick={() => createMedia()}
-                        className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
-                    >{submit}</button>
+                <div className="bg-[#fff] rounded-lg px-5 py-10 shadow-xl dark:bg-transparent">
+                    <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Description
+                            </label>
+                            <textarea rows="1" onChange={handleDescription} placeholder="Enter Description" class="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"></textarea>
+                        </div>
 
-                </div>
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Link
+                            </label>
+                            <input
+                                type="file"
+                                onChange={handleImage}
+                                className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary"
+                            />
+                        </div>
+
+                    </div>
+                    <div className="w-50 mx-auto mt-5">
+                        <button onClick={() => createMedia()}
+                            className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
+                        >{submit}</button>
+
+                    </div>
                 </div>
 
             </DefaultLayout>
