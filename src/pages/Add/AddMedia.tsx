b32297df09dedefe9bcbee5253f8fbb9c8a54931
lalitharwateappdid <lalitharwateappdid@gmail.com>
2024-06-20 22:40:01 +0100

    youtube working perfectly

--- src/pages/Add/AddMedia.tsx
+++ src/pages/Add/AddMedia.tsx
@@ -12,17 +12,17 @@ const AddMedia = () => {
 
     const [title, setTitle] = useState("");
     const [link, setLink] = useState("");
-    const [submit,setSubmit] = useState("Submit")
-  
+    const [submit, setSubmit] = useState("Submit")
+
 
     // function to store books
     const createMedia = async () => {
-        console.log(title,link);
+        console.log(title, link);
         try {
             setSubmit("Submitting...")
             const response = await axios.post(`${apiLink}media/create`, {
-                title:title,
-                link:link
+                title: title,
+                link: link
             });
             Notify(response.data.message);
             setTitle("")
@@ -47,40 +47,40 @@ const AddMedia = () => {
 
             <DefaultLayout>
                 <Breadcrumb pageName="Add Youtube Media" />
-                <div className='bg-[#fff] px-5 py-3 shadow-xl rounded-lg dark:bg-transparent'>
-                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
-                    <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                    </div>
-                    <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Link
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Link"
-                            value={link}
-                            onChange={(e) => setLink(e.target.value)}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                    </div>
-                   
-                </div>
-                <div className="w-50 mx-auto mt-5">
-                    <button onClick={() => createMedia()}
-                        className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
-                    >{submit}</button>
+                <div className='bg-[#fff] px-5 py-10 shadow-xl rounded-lg dark:bg-transparent'>
+                    <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Title
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter Title"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                        </div>
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Link
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter Link"
+                                value={link}
+                                onChange={(e) => setLink(e.target.value)}
+                                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                        </div>
 
-                </div>
+                    </div>
+                    <div className="w-50 mx-auto mt-5">
+                        <button onClick={() => createMedia()}
+                            className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
+                        >{submit}</button>
+
+                    </div>
                 </div>
 
             </DefaultLayout>
