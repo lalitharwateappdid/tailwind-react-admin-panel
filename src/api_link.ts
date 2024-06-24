a4f4f7d3b9d526ada54b4171768341c137aec6a8
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-24 17:14:36 +0530

    updated

--- src/api_link.ts
+++ src/api_link.ts
@@ -1,2 +1,2 @@
-export const apiLink = 'https://localhost:5000/api/';
-export const apiImageLink = 'https://localhost:5000/'
+export const apiLink = 'http://192.168.1.37:5000/api/';
+export const apiImageLink = 'http://192.168.1.37:5000/'
--- src/pages/Add/BusinessSettings.tsx
+++ src/pages/Add/BusinessSettings.tsx
@@ -6,36 +6,60 @@ import { apiLink } from '../../api_link';
 import Swal from 'sweetalert2';
 import { InputSwitch } from "primereact/inputswitch";
 import axios from 'axios';
+import MarkdownEditor from '@uiw/react-markdown-editor';
+
+
 
 const BusinessSettings = () => {
 
 
-    const [title, setTitle] = useState("");
-    const [description, setDescription] = useState("");
+    // const [title, setTitle] = useState("");
+    const [aboutTitle,setAboutTitle] = useState("")
+    // const [description, setDescription] = useState("");
+    const [aboutDescription,setAboutDescription] = useState("")
     const [submit,setSubmit] = useState("Submit")
+    const [youtube,setYoutube] = useState("")
+    const [website,setWebsite] = useState("")
+    const [privacyPolicy, setPrivacyPolicy] = useState("")
+    const [terms,setTerms] = useState("")
+    const [contact,setContact] = useState("")
+    const [android_version,SetAndroidVersion] = useState("") 
+    const [image,setAboutImage] = useState("")
+    const [app,setApp] = useState("")
+    const [forceUpdate,setForceUpdate] = useState("")
+
+    const handleImage = (e) => {
+        setAboutImage(e.target.files[0]);
+    }
   
     // function to store books
     const createMedia = async () => {
 
         try {
             const formData = new FormData();
-            formData.append("title", title)
-            formData.append("description", description);
-
-            // formData.append("id",`${id}`)
+            formData.append("about_title", aboutTitle)
+            formData.append("about_description", aboutDescription);
+            formData.append("youtube",youtube);
+            formData.append("website",website)
+            formData.append("privacyPolicy",privacyPolicy)
+            formData.append("terms",terms)
+            formData.append("contact",'contact')
+            formData.append("android_version",'android_version')
+            formData.append("image",image);
+            formData.append("app_link",app)
 
             setSubmit("Submitting...")
 
             const response = await axios.post(`${apiLink}business-settings/create`, formData, {
                 headers: {
-                    "Content-Type": "multipart/form-data"
+                    "Content-Type": "multipart/form-data",
+                    "Accept":"appplication/json"
                 }
             });
 
 
             Notify(response.data.message);
-            setTitle("")
-            setDescription("")
+           
             
             setSubmit("Submit")
         }
@@ -55,8 +79,20 @@ const BusinessSettings = () => {
             try {
                 const category_response = await fetch(`${apiLink}business-settings/get`);
                 const category_data = await category_response.json();
-                // console.log(category_data);
-                setTitle(category_data)
+                console.log(category_data.data);
+                setAboutTitle(category_data.data['about_title'])
+                setAboutDescription(category_data.data['about_description'])
+                setYoutube(category_data.data['youtube']);
+                setWebsite(category_data.data['website'])
+                setPrivacyPolicy(category_data.data['privacyPolicy'])
+                setTerms(category_data.data['terms'])
+                setContact(category_data.data['contact'])
+                SetAndroidVersion(category_data.data['android_version'])
+                setApp(category_data.data['app_link'])
+                set
+                
+                
+
 
                 // setCategoryData(category_data.data);
             } catch (error) {
@@ -76,7 +112,7 @@ const BusinessSettings = () => {
             <DefaultLayout>
                 <Breadcrumb pageName="Business Settings" />
                 <div className="shadow-xl py-10 px-5 bg-[#fff] rounded-lg dark:bg-transparent">
-
+                
 
                     {/* android section starts */}
                     <div className="grid lg:grid-cols-3 sm:grid-cols-1 md:grid-cols-2 gap-8">
@@ -87,7 +123,8 @@ const BusinessSettings = () => {
                         <input
                             type="text"
                             placeholder="Enter Android Version"
-                            
+                            value={android_version}
+                            onChange={(e) => SetAndroidVersion(e.target.value)}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -96,7 +133,7 @@ const BusinessSettings = () => {
                         <label className="mb-3 block text-black dark:text-white">
                             Force Update
                         </label>
-                        <InputSwitch className="p-invalid" checked={true} />
+                        <InputSwitch className="p-invalid" checked={forceUpdate == "1" ? true: false} />
                         
                     </div>
 
@@ -120,7 +157,8 @@ const BusinessSettings = () => {
                         <input
                             type="text"
                             placeholder="Enter About Title"
-                            
+                            value={aboutTitle}
+                            onChange={(e) => setAboutTitle(e.target.value)}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -130,9 +168,9 @@ const BusinessSettings = () => {
                             Image
                         </label>
                         <input
-                            type="text"
+                            type="upload"
                             placeholder="Enter About Title"
-                            
+                            onChange={handleImage}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -144,7 +182,12 @@ const BusinessSettings = () => {
                             Description
                         </label>
                       
-                         <textarea rows="3" placeholder="Enter Description" class="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"></textarea>
+                                         
+                        <MarkdownEditor
+      value={aboutDescription}
+      height='200px'
+      onChange={(e) => {setAboutDescription(e.target.value)}}
+    />
                     </div>
                       </div>
 
@@ -159,8 +202,9 @@ const BusinessSettings = () => {
                         
                         <input
                             type="text"
-                            placeholder="Enter About Title"
-                            
+                            placeholder="Add Youtube Link"
+                            value={youtube}
+                            onChange={(e) => setYoutube(e.target.value)}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -177,12 +221,29 @@ const BusinessSettings = () => {
                         <input
                             type="text"
                             placeholder="Enter Website Link "
-                            
+                            value={website}
+                            onChange={(e)=>setWebsite(e.target.value)}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
                       </div>
                     {/* website section ends */}
+
+                    {/* app link section starts */}
+                    <h3 className="dark:text-white font-bold underline text-center my-5">App Link</h3>
+                    <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
+                    <div>
+                       
+                        <input
+                            type="text"
+                            placeholder="Add App Link "
+                            value={app}
+                            onChange={(e => setApp(e.target.value))}
+                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                    </div>
+                      </div>
+                    {/* app link section ends */}
 
                     {/* privacy policy section starts */}
                     <h3 className="dark:text-white font-bold underline text-center my-5">Privacy Policy</h3>
@@ -192,7 +253,8 @@ const BusinessSettings = () => {
                         <input
                             type="text"
                             placeholder="Enter Privacy Policy Link "
-                            
+                            value={privacyPolicy}
+                            onChange={(e) => setPrivacyPolicy(e.target.value)}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -207,7 +269,8 @@ const BusinessSettings = () => {
                         <input
                             type="text"
                             placeholder="Enter Terms & Condition  Link "
-                            
+                            value={terms}
+                            onChange={(e => setTerms(e.target.value))}
                             className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
                         />
                     </div>
@@ -216,7 +279,8 @@ const BusinessSettings = () => {
 
                     {/* contact us section starts */}
                     <h3 className="dark:text-white font-bold underline text-center my-5">Contact Us</h3>
-
+                    <MarkdownEditor
+                    height='200px' value={contact} onChange={(e => setContact(e.target.value))} />
                     {/* contact us section ends */}
 
 
