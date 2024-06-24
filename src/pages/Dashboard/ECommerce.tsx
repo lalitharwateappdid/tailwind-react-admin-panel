5bb28d0e011ebd32528ff6e4e3920acdac222a2b
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-06-24 18:51:34 +0530

    updated

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -43,7 +43,7 @@ const ECommerce: React.FC = () => {
 
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
         <Link to="/home-content">
-          <CardDataStats title="Total Sliders" total={slider} rate="" >
+          <CardDataStats title="Total Sliders again" total={slider} rate="" >
             <span className="material-symbols-outlined">
               inventory_2
             </span>
