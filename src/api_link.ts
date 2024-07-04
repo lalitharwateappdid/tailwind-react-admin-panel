d46219d90bef7b0c45add4be97b055a481c3c494
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-07-04 15:27:39 +0530

    updated

--- src/api_link.ts
+++ src/api_link.ts
@@ -3,7 +3,7 @@ export const apiLink = 'https://api.shripethekaka.com/api/';
 export const apiImageLink = 'https://api.shripethekaka.com/';
 
 
-// local
+// local 
 // export const apiLink = 'http://localhost:5000/api/';
 // export const apiImageLink = 'http://localhost:5000/';
 
