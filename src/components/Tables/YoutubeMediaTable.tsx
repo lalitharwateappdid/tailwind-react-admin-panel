962f9554511a56efe0cc4e21120e2611970cc65b
lalitharwateappdid <lalitharwateappdid@gmail.com>
2024-07-06 01:43:07 +0530

    updated

--- src/components/Tables/YoutubeMediaTable.tsx
+++ src/components/Tables/YoutubeMediaTable.tsx
@@ -7,7 +7,7 @@ import { Column } from "primereact/column";
 import { InputSwitch } from "primereact/inputswitch";
 import { Link } from "react-router-dom";
 import Notify from "../toast_notify/Notify";
-
+import ModalImage from "react-modal-image";
 
 function ListTable() {
     const [apiData, setApiData] = useState([]);
@@ -62,43 +62,55 @@ function ListTable() {
         }
     }
 
-    const handleStatus = async(id) => {
-        try{
-            const response = await axios.put(`${apiLink}media/status`,{
-                id:id
+    const handleStatus = async (id) => {
+        try {
+            const response = await axios.put(`${apiLink}media/status`, {
+                id: id
             })
             fetchData()
             Notify(response.data.message)
-            
+
         }
-        catch(err){
+        catch (err) {
             console.error("Something went wrong " + err)
         }
     }
 
-   
+
 
     return (
         <>
             <div className="float-right mb-4">
                 <Link to="/add-media" className="bg-primary text-white px-3 py-2 rounded-md hover:opacity-65">Add</Link>
             </div>
-            
-            <br/>
+
+            <br />
             <br />
 
             <DataTable paginatorClassName={"dark:bg-[#243141] dark:text-[#fff]"} value={apiData} className="shadow-xl" stripedRows paginator rows={10}
                 rowsPerPageOptions={[5, 10, 25, 50]}
                 tableStyle={{ minWidth: '50rem' }}>
-                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="id" header="Sr.No" body={(item,key) => (
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="id" header="Sr.No" body={(item, key) => (
                     <>
-                    <span>{key.rowIndex + 1}</span>
+                        <span>{key.rowIndex + 1}</span>
                     </>
                 )} ></Column>
-            
+
                 <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="title" header="Title" />
                 <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="link" header="Link" />
-              
+                <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="thumbnail" header="Video Thumbnail"
+                    body={(item, key) => (
+                        <>
+                            <ModalImage className="w-20  z-9999"
+                                small={`${item.thumbnail}`}
+                                large={`${item.thumbnail}`}
+
+                            />
+                        </>
+                    )}
+                />
+
+
                 <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="status" header="Status" body={(rowData) => (
                     <InputSwitch className="p-invalid" checked={rowData.status} onClick={() => handleStatus(rowData.id)} />
                 )
@@ -107,13 +119,13 @@ function ListTable() {
                 <Column headerClassName={"dark:text-[#fff]  dark:border-[#fff]  dark:bg-[#243141]"} bodyClassName={"dark:text-[#fff] dark:border-[#ffffff13]  dark:bg-[#243141]"} field="id" header="Action" body={(rowData) => (
                     <div className="flex gap-2">
                         <Link to={`/edit-media/${rowData.id}`} className="bg-primary opacity-90  p-2     text-white rounded-full hover:opacity-100 "><i className="fa-solid fa-pen"></i></Link>
-                        <span onClick={() =>  handleDelete(rowData.id)} className="bg-red-700 opacity-90 hover:opacity-100 p-2  rounded-full text-white" ><i className="fa-solid fa-trash"></i></span>
+                        <span onClick={() => handleDelete(rowData.id)} className="bg-red-700 opacity-90 hover:opacity-100 p-2  rounded-full text-white" ><i className="fa-solid fa-trash"></i></span>
                     </div>
                 )} />
 
             </DataTable>
 
- 
+
         </>
 
     );
--- src/pages/Update/UpdateMedia.tsx
+++ src/pages/Update/UpdateMedia.tsx
@@ -1,53 +1,66 @@
 import Breadcrumb from '../../components/Breadcrumbs/Breadcrumb';
 import DefaultLayout from '../../layout/DefaultLayout';
 import Notify from '../../components/toast_notify/Notify';
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { apiLink } from '../../api_link';
 import Swal from 'sweetalert2';
 import axios from 'axios';
+import ModalImage from "react-modal-image";
 
 
 const UpdateMedia = () => {
 
 
-        const { id } = useParams();
-        
-        const [title, setTitle] = useState('');
-        const [link, setLink] = useState('');
-    
-        const [update,setUpdate] = useState("Update")
-      
-        const getData =  async () => {
-            try{
-                const response = await axios.get(`${apiLink}media/edit/${id}`);
-                const data = response.data.data
-                console.log(data)
-                setTitle(data.title)
-                setLink(data.link)
-            }
-            catch(err){
-                console.log(err);
-            }
+    const { id } = useParams();
+
+    const [title, setTitle] = useState('');
+    const [link, setLink] = useState('');
+    const [thumbnail, setThumbnail] = useState('')
+
+    const [update, setUpdate] = useState("Update")
+
+    const handleFileChange = (e) => {
+        setThumbnail(e.target.files[0])
     }
 
-    useEffect(()=>{
+    const getData = async () => {
+        try {
+            const response = await axios.get(`${apiLink}media/edit/${id}`);
+            const data = response.data.data
+            // console.log(data)
+            setTitle(data.title)
+            setLink(data.link)
+            setThumbnail(data.thumbnail)
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    useEffect(() => {
         getData();
-    },[]);
+    }, []);
 
     const handleUpdate = async () => {
-        try{
+        try {
+            const formData = new FormData()
+
+            formData.append("id", id)
+            formData.append("title", title)
+            formData.append("link", link)
+            formData.append("thumbnail", thumbnail)
             setUpdate("Updating...")
-            const response = await axios.put(`${apiLink}media/update`,{
-                id:id,
-                title:title,
-                link:link
+            const response = await axios.put(`${apiLink}media/update`, formData, {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
             });
             Notify(response.data.message);
             setUpdate("Update")
 
         }
-        catch(err){
+        catch (err) {
             setUpdate("Update")
             Swal.fire({
                 title: "Error!",
@@ -56,48 +69,60 @@ const UpdateMedia = () => {
             });
         }
     }
-        
 
-       
+
+
     return (
-       
+
         <>
             <DefaultLayout>
                 <Breadcrumb pageName="Update Media " />
 
                 <div className="bg-[#fff] rounded-lg px-5 py-10 shadow-xl dark:bg-transparent">
-                <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
-                    <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Title
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                    </div>
-                    <div>
-                        <label className="mb-3 block text-black dark:text-white">
-                            Link
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Link"
-                            value={link}
-                            onChange={(e) => setLink(e.target.value)}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                   </div>
-                </div>
-                <div className="w-50 mx-auto mt-5">
-                    <button onClick={() => handleUpdate()}
-                        className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
-                    >{update}</button>
+                    <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Title
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter Title"
+                                value={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                        </div>
+                        <div>
+                            <label className="mb-3 block text-black dark:text-white">
+                                Link
+                            </label>
+                            <input
+                                type="text"
+                                placeholder="Enter Link"
+                                value={link}
+                                onChange={(e) => setLink(e.target.value)}
+                                className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                            />
+                        </div>
 
-                </div>
+                        <div>
+                            <ModalImage className="w-[140px] mb-4  z-9999"
+                                small={thumbnail}
+                                large={thumbnail}
+
+                            />
+                            <label className="mb-3 block text-black dark:text-white">
+                                Youtube Thumbnail
+                            </label>
+                            <input type="file" onChange={handleFileChange} className="w-full cursor-pointer rounded-lg border-[1.5px] border-stroke bg-transparent outline-none transition file:mr-5 file:border-collapse file:cursor-pointer file:border-0 file:border-r file:border-solid file:border-stroke file:bg-whiter file:py-3 file:px-5 file:hover:bg-primary file:hover:bg-opacity-10 focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:file:border-form-strokedark dark:file:bg-white/30 dark:file:text-white dark:focus:border-primary" />
+                        </div>
+                    </div>
+                    <div className="w-50 mx-auto mt-5">
+                        <button onClick={() => handleUpdate()}
+                            className="flex justify-center font-bold rounded-lg bg-primary text-white text-center w-50  py-3"
+                        >{update}</button>
+
+                    </div>
                 </div>
 
             </DefaultLayout>
