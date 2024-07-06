f942f84025144630bb5e16e61a8a24e683197c1d
lalitharwateappdid <lalitharwateappdid@gmail.com>
2024-07-06 12:00:17 +0530

    updated

--- src/pages/Add/AddCategory.tsx
+++ src/pages/Add/AddCategory.tsx
@@ -12,10 +12,7 @@ import Select from 'react-select'
 
 const AddCategory = () => {
 
-    const countries = [
-        { value: 'me', label: 'Montenegro', image: 'https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' },
-        { value: 'rs', label: 'Serbia', image: 'https://plus.unsplash.com/premium_photo-1676637000058-96549206fe71?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' }
-    ];
+
 
 
     const [name, setName] = useState("");
@@ -27,13 +24,19 @@ const AddCategory = () => {
     const [categoryImage, setCategoryImage] = useState([])
     const [categoryDropdown, setSubCategoryDropdown] = useState([])
 
+
+
     // function to store books
     const createBook = async () => {
         try {
+
+            
             setSubmit("Submitting...")
+            
             const response = await axios.post(`${apiLink}category/create`, {
                 name: name,
-                description: description
+                description: description,
+
             });
             Notify(response.data.message);
             setName('');
@@ -54,8 +57,9 @@ const AddCategory = () => {
 
 
     const fetchData = async () => {
-        const response = await axios.get(`${apiLink}category/get`);
+        const response = await axios.get(`${apiLink}category/get-category`);
         const result = await response.data.data
+        console.log(result)
         setSubCategoryDropdown(result)
     }
 
@@ -70,7 +74,7 @@ const AddCategory = () => {
     useEffect(() => {
         fetchData()
         fetchCategoryImage()
-    })
+    }, [])
 
 
 
@@ -176,52 +180,48 @@ const AddCategory = () => {
 
 
                         {
-                            child ?
+                            child &&
 
-                                <div>
-                                    <label className="mb-2.5 block text-black dark:text-white">
-                                        Select Child Category
+                            <div>
+                                <label className="mb-2.5 block text-black dark:text-white">
+                                    Select Child Category
 
-                                    </label>
+                                </label>
 
-                                    <div className=" z-20 bg-transparent dark:bg-form-input">
-                                        <select value={category} onChange={(e) => setCategory(e.target.value)}
-                                            className="relative z-20 w-full appearance-none rounded border border-stroke bg-transparent py-3 px-5 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+                                <div className=" z-20 bg-transparent dark:bg-form-input">
+                                    <select className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary">
+                                        {categoryDropdown && categoryDropdown.length > 0 ? (
+                                            categoryDropdown.map((category) => (
+                                                <option key={category.id} value={category.id} className="text-body dark:text-bodydark">
+                                                    {category.title}
+                                                </option>
+                                            ))
+                                        ) : (
+                                            <option disabled>No categories available</option>
+                                        )}
+                                    </select>
+
+                                    <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
+                                        <svg
+                                            className="fill-current"
+                                            width="24"
+                                            height="24"
+                                            viewBox="0 0 24 24"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
                                         >
-                                            <option value="" disabled selected className="text-body dark:text-bodydark">
-                                                Select Child Category
-                                            </option>
-                                            {
-                                                categoryDropdown.map((category) => (
-                                                    <option key={category.id} value={category.id} className="text-body dark:text-bodydark">
-                                                        {category.name}
-                                                    </option>
-                                                ))
-                                            }
-
-                                        </select>
-
-                                        <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
-                                            <svg
-                                                className="fill-current"
-                                                width="24"
-                                                height="24"
-                                                viewBox="0 0 24 24"
-                                                fill="none"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <g opacity="0.8">
-                                                    <path
-                                                        fillRule="evenodd"
-                                                        clipRule="evenodd"
-                                                        d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                                                        fill=""
-                                                    ></path>
-                                                </g>
-                                            </svg>
-                                        </span>
-                                    </div>
-                                </div> : ""
+                                            <g opacity="0.8">
+                                                <path
+                                                    fillRule="evenodd"
+                                                    clipRule="evenodd"
+                                                    d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
+                                                    fill=""
+                                                ></path>
+                                            </g>
+                                        </svg>
+                                    </span>
+                                </div>
+                            </div>
                         }
 
                     </div>
