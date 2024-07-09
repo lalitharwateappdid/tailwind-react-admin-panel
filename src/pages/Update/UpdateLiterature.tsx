e9f8e9499083be4d3c79b323c352a6f6bfff4092
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-07-09 14:42:04 +0530

    updated

--- src/pages/Update/UpdateLiterature.tsx
+++ src/pages/Update/UpdateLiterature.tsx
@@ -133,18 +133,6 @@ const UpdateLiterature = () => {
                     {/* category_id dropdown */}
                     <div>
                         <label className="mb-2.5 block text-black dark:text-white">
-                            Sr No
-                        </label>
-                        <input
-                            type="text"
-                            placeholder="Enter Sr No"
-                            value={srno}
-                            onChange={(e) => setSrno(e.target.value)}
-                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                        />
-                    </div>
-                    <div>
-                        <label className="mb-2.5 block text-black dark:text-white">
                             Select Category
 
                         </label>
@@ -187,6 +175,32 @@ const UpdateLiterature = () => {
                             </span>
                         </div>
                     </div>
+
+                    <div>
+                        <label className="mb-2.5 block text-black dark:text-white">
+                            Literature Content
+                        </label>
+                        <ReactQuill
+                            className='h-[150px] rounded-lg mb-20'
+                            theme="snow"
+                            value={literatureContent}
+                            onChange={handleContent}
+                        />
+                    </div>
+
+                    <div>
+                        <label className="mb-2.5 block text-black dark:text-white">
+                            Sr No
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Sr No"
+                            value={srno}
+                            onChange={(e) => setSrno(e.target.value)}
+                            className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+                        />
+                    </div>
+                    
                     {/* category_id dropdown ends */}
 
 
