f058e1e620ebdfe4eb929cb86c62c137a2dd7aff
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-07-09 14:48:44 +0530

    updated

--- src/pages/Update/UpdateLiterature.tsx
+++ src/pages/Update/UpdateLiterature.tsx
@@ -176,17 +176,6 @@ const UpdateLiterature = () => {
                         </div>
                     </div>
 
-                    <div>
-                        <label className="mb-2.5 block text-black dark:text-white">
-                            Literature Content
-                        </label>
-                        <ReactQuill
-                            className='h-[150px] rounded-lg mb-20'
-                            theme="snow"
-                            value={literatureContent}
-                            onChange={handleContent}
-                        />
-                    </div>
 
                     <div>
                         <label className="mb-2.5 block text-black dark:text-white">
