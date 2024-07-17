66c0aae85b74355300c7df9f532fa4b576935c03
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-07-17 16:57:37 +0530

    updated

--- src/pages/Add/AddCategory.tsx
+++ src/pages/Add/AddCategory.tsx
@@ -28,6 +28,8 @@ const AddCategory = () => {
     const [categoryImage, setCategoryImage] = useState([])
     const [categoryDropdown, setSubCategoryDropdown] = useState([])
 
+    const [toggleForm,setToggleForm] = useState(false)
+
     const handleImage = (value) => {
         setImage(value)
     }
@@ -185,9 +187,16 @@ const AddCategory = () => {
 
 
             <DefaultLayout>
-                <Breadcrumb pageName="Add Category" />
+                <Breadcrumb pageName={
+                    toggleForm==false? "Add Category" : "Map Category"
+                } />
+                <div className='p-2 flex justify-center'>
+                    <span className='mr-2 font-semibold'>Add Category</span>    <InputSwitch checked={toggleForm==false? false:true} onChange={(e) => setToggleForm(e.target.value)}/> <span className='ml-2 font-semibold'>Map Category</span>
+                </div>
+                {
+                    toggleForm==false ? 
 
-                <div className="px-5 py-10 rounded-lg bg-[#fff] dark:bg-transparent shadow-xl">
+                    <div className="px-5 py-10 rounded-lg bg-[#fff] dark:bg-transparent shadow-xl">
                     <div className="grid lg:grid-cols-2 sm:grid-cols-1 md:grid-cols-2 gap-8">
 
                         <div>
@@ -233,50 +242,6 @@ const AddCategory = () => {
                         </div>
 
 
-                        {/* {
-                            child &&
-
-                            <div>
-                                <label className="mb-2.5 block text-black dark:text-white">
-                                    Select Child Category
-
-                                </label>
-
-                                <div className=" z-20 bg-transparent dark:bg-form-input">
-                                    <select onChange={(e) => { setChildCategory(e.target.value) }} className="w-full rounded-lg border-[1.5px] border-stroke bg-transparent py-3 px-5 text-black outline-none transition focus:border-primary active:border-primary disabled:cursor-default disabled:bg-whiter dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary">
-                                        {categoryDropdown && categoryDropdown.length > 0 ? (
-                                            categoryDropdown.map((category) => (
-                                                <option key={category.id} value={category.id} className="text-body dark:text-bodydark">
-                                                    {category.title}
-                                                </option>
-                                            ))
-                                        ) : (
-                                            <option disabled>No categories available</option>
-                                        )}
-                                    </select>
-
-                                    <span className="absolute top-1/2 right-4 z-30 -translate-y-1/2">
-                                        <svg
-                                            className="fill-current"
-                                            width="24"
-                                            height="24"
-                                            viewBox="0 0 24 24"
-                                            fill="none"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <g opacity="0.8">
-                                                <path
-                                                    fillRule="evenodd"
-                                                    clipRule="evenodd"
-                                                    d="M5.29289 8.29289C5.68342 7.90237 6.31658 7.90237 6.70711 8.29289L12 13.5858L17.2929 8.29289C17.6834 7.90237 18.3166 7.90237 18.7071 8.29289C19.0976 8.68342 19.0976 9.31658 18.7071 9.70711L12.7071 15.7071C12.3166 16.0976 11.6834 16.0976 11.2929 15.7071L5.29289 9.70711C4.90237 9.31658 4.90237 8.68342 5.29289 8.29289Z"
-                                                    fill=""
-                                                ></path>
-                                            </g>
-                                        </svg>
-                                    </span>
-                                </div>
-                            </div>
-                        } */}
 
                     </div>
                     <div className="w-50 mx-auto mt-5 mb-10">
@@ -285,10 +250,10 @@ const AddCategory = () => {
                         >{submit}</button>
 
                     </div>
+                    </div> :
 
-                    <hr className='mt-10 mb-10' />
-
-                    <div className='grid md:grid-cols-2 gap-8'>
+ <div className="px-5 py-10 rounded-lg bg-[#fff] dark:bg-transparent shadow-xl">
+<div className='grid md:grid-cols-2 gap-8'>
 
                         <div>
                             <label className="mb-2.5 block text-black dark:text-white">
@@ -378,7 +343,12 @@ const AddCategory = () => {
                         >{submit}</button>
 
                     </div>
-                </div>
+                    </div>
+             
+
+
+                }
+               
 
             </DefaultLayout >
 
